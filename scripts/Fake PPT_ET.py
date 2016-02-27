657f1ada93327f2d4886ad40921f617ae73656c5
prasanna310 <dahal.prasanna@gmail.com>
2016-02-27 14:07:42 -0700

    Pytopkapi simulations files

---
+++ scripts/Fake PPT_ET.py
@@ -0,0 +1,187 @@
+
+
+##### my code #####
+import os, h5py , numpy
+os.chdir(r'C:\Users\Prasanna\Google Drive\SharedWithDrCastronova\2016\PyCharmWorkspace\FEB\NL_C03_2\run_the_model\forcing_variables')
+
+time_step = 170
+no_of_cell = 1885
+
+with h5py.File('rainfields_sampleWatershed.h5','w') as f2:
+    f2.create_group('sample_event')
+    f2['sample_event'].create_dataset('rainfall', shape=(time_step, no_of_cell), dtype='f' )
+
+    rainArray =  f2['sample_event']['rainfall']
+    data = []
+    for i in range(time_step):
+        one_time_ppt_all_cell = numpy.random.rand(no_of_cell,1)*0
+        data.append(one_time_ppt_all_cell)
+
+    rainArray = data
+
+
+
+with h5py.File('ET_sampleWatershed.h5','w' ) as f1:
+    f1.create_group('sample_event')
+    f1['sample_event'].create_dataset('ETo', shape=( time_step, no_of_cell), dtype='f' )
+    f1['sample_event'].create_dataset('ETr', shape=( time_step, no_of_cell), dtype='f' )
+
+    EToArray =  f1['sample_event']['ETo']
+    ETrArray =  f1['sample_event']['ETr']
+
+    data = []
+    for i in range(time_step):
+        one_time_ppt_all_cell = numpy.random.rand(no_of_cell,1)*0
+        data.append(one_time_ppt_all_cell)
+
+    EToArray = data
+    ETrArray = data
+
+
+#
+#
+# #reading usmmary
+# et_h5 = h5py.File("ET.h5" , "r")
+# et_h5['sample_event']['ETr'][:]
+#
+# rain_h5 = h5py.File('rainfields.h5')
+# rain_h5['sample_event']['rainfall'][:]
+#
+# h5py.File('rainfields_sampleWatershed.h5',"r")['sample_event']['rainfall']
+# h5py.File('rainfields_sampleWatershed.h5',"r").close()
+#
+# ###########
+# # READING #
+# ###########
+# os.chdir(r'C:\Users\Prasanna\Google Drive\SharedWithDrCastronova\2016\PyCharmWorkspace\FEB\NL_C03_2\run_the_model\forcing_variables')
+#
+# # open the file that we created above
+# f = h5py.File("ET.h5", "r")
+# print '\nf is an h5py File: ',(type(f) == h5py.File)
+# print 'f contains elements: ',f.keys()
+#
+# # get the group
+# grp = f['sample_event']
+# print '\ngrp is an h5py Group: ',(type(grp) == h5py.Group)
+# print 'grp contains elements: ', grp.keys()
+#
+# # get the dataset
+# dset = grp['ETo']
+# print '\ndset is an h5py File: ',(type(dset) == h5py.Dataset)
+# print 'dset has a shape of: ', dset.shape
+#
+# # get the data using array indexing
+# print '\t',dset[:]
+# print '\t',dset[:-4]
+# print '\t',dset[:5]
+# print '\t',dset[0:2]
+#
+# f.close()
+#
+#
+#
+#
+#
+#
+#
+# # ############################################
+# # #Reading and writing h5, by Dr. Castronova
+# ############################################
+# #
+# # # This script demonstrates how to build hdf5 files from timeseries data
+# # # Most of this content was derived from http://docs.h5py.org/en/latest/quick.html
+# #
+# #
+# # import h5py
+# #
+# # ###########
+# # # WRITING #
+# # ###########
+# #
+# # # create an empty hdf5 file
+# # f = h5py.File("mytestfile.h5","w")
+# #
+# # # create a group
+# # #   * a group is a container that can hold multiple datasets
+# # grp = f.create_group("climate_data")
+# #
+# # # add a dataset to the group
+# # #   * (10,) indicates the size of the data (rows, cols)
+# # #   * dtype indicates the type of data that will be stored 'f' = float
+# # dset = grp.create_dataset('prcp', (10,), dtype='f')
+# #
+# # # NOTE: the group and dataset can be accomplished in a single statement
+# # #   create_dataset('climate_data/prcp').
+# #
+# # # add some data values to the dataset
+# # values = range(10)
+# # dset[:] = values
+# #
+# # f.close()
+# #
+# # # ----------------------------------------------------------------------
+# #
+# # ###########
+# # # READING #
+# # ###########
+# #
+# # # open the file that we created above
+# # f = h5py.File("mytestfile.h5", "r")
+# # print '\nf is an h5py File: ',(type(f) == h5py.File)
+# # print 'f contains elements: ',f.keys()
+# #
+# # # get the group
+# # grp = f['climate_data']
+# # print '\ngrp is an h5py Group: ',(type(grp) == h5py.Group)
+# # print 'grp contains elements: ', grp.keys()
+# #
+# # # get the dataset
+# # dset = grp['prcp']
+# # print '\ndset is an h5py File: ',(type(dset) == h5py.Dataset)
+# # print 'dset has a shape of: ', dset.shape
+# #
+# # # get the data using array indexing
+# # print '\t',dset[:]
+# # print '\t',dset[:-4]
+# # print '\t',dset[:5]
+# # print '\t',dset[0:2]
+# #
+# # f.close()
+# #
+# # # ----------------------------------------------------------------------
+# #
+# # ####################
+# # # ADVANCED WRITING #
+# # ####################
+# #
+# # import numpy
+# #
+# # # create some data
+# # data = numpy.random.rand(10,10)
+# #
+# # # open the file
+# # with h5py.File('mytestfile2.h5', 'w') as f:
+# #     # create group and dataset in one line
+# #     dset = f.create_dataset('climate_data/prcp', shape=(10,10), dtype='f')
+# #
+# #     # set the datavalues for this group
+# #     dset[:] = data
+# #
+# # # check that the data was set properly
+# # with h5py.File("mytestfile2.h5", "r") as f:
+# #     print '\nf is an h5py File: ',(type(f) == h5py.File)
+# #     print 'f contains elements: ',f.keys()
+# #
+# #     grp = f['climate_data']
+# #     print '\ngrp is an h5py Group: ',(type(grp) == h5py.Group)
+# #     print 'grp contains elements: ', grp.keys()
+# #
+# #     dset = grp['prcp']
+# #     print '\ndset is an h5py File: ',(type(dset) == h5py.Dataset)
+# #     print 'dset has a shape of: ', dset.shape
+# #
+# #     print '\nData Querying'
+# #     print dset[0,:]
+# #     print '\n', dset[1:4,1:4]
+# #     print '\n',dset[:,1]
+# #
---
+++ scripts/make_dat_5.py
@@ -0,0 +1,136 @@
+import sys
+import time
+import numpy
+from osgeo import gdal
+import argparse
+import os
+
+def ascii2row(asciiRaster):
+    itemsInaList=[]
+    c=1
+    with open(asciiRaster) as fp:
+        for line in fp:
+            c=c+1
+            tempList = line.strip().split(" ")
+            if c > 7:
+                for item in tempList:
+                    if item != str(-9999):
+                        itemsInaList.append(item)
+
+    data = numpy.array(itemsInaList)
+
+    return data.reshape(data.shape[0], 1)
+
+def flatten_tiff(tifpath):
+    # read the tif file
+    ds = gdal.Open(tifpath)
+
+    # read all the data
+    data = numpy.array(ds.GetRasterBand(1).ReadAsArray())
+
+    # filter out the nodata values
+    data = data[data >= -100]
+
+    # reshape the data into a single column
+    data = data.reshape(data.shape[0], 1)
+
+    return data
+
+
+#----
+path_2_tiffs = r"C:\Users\Prasanna\Box Sync\Red Butte Creek rasters2\TIFFs"
+path_2_tiffs= r"C:\Users\Prasanna\Box Sync\Red Butte Creek rastrers6\Tiffs"
+path_2_tiffs= r"C:\Users\Prasanna\Google Drive\SharedWithDrCastronova\2016\PyCharmWorkspace\FEB\NL_C03_2\create_the_parameter_files\\Tiff"
+
+input_files = ["mask2.tif", "str.tif" ,'DEM_Prj.tif', 'fdr.tif',  "drp.tif", "soildepth.tif", 'resmoisture.tif' ,
+               'effporosity.tif','n_Overland.tif', 'n_Channel.tif', 'Land_Use_Prj.tif', 'poresize.tif',
+               'bblingpr.tif' , 'kc_fake.tif', 'ksat.tif']
+
+# list of TIF in a different way, all tif from a specified folder
+os.chdir(path_2_tiffs)
+all_Files = os.listdir(path_2_tiffs)
+input_files = []
+for file in all_Files:
+    if file.split(".")[-1] == 'tif':
+        input_files.append(file)
+
+
+output_file = "cell_param.dat"
+
+if os.path.exists(output_file):
+    response = raw_input('Output file already exists. Do you want to delete it? [Y/n]')
+
+    # delete the file
+    if response != 'n':
+        os.remove(output_file)
+data = []
+
+for a_File in input_files:
+    data.append(flatten_tiff(a_File))
+    print "%s is written and contains %s values " % (a_File,len(flatten_tiff(a_File)))
+
+
+# write the output
+with open(output_file, 'w') as f:
+    for i in range(len(data)):
+        if data[i].dtype != 'float':
+            data[i] = data[i].astype('float')
+
+    # build the output array
+    outarr = data[0]
+    for i in range(1, len(data)):
+        outarr = numpy.hstack([outarr, data[i]])
+
+    # save outarr to text file
+    numpy.savetxt(f, outarr, fmt='%1.3f', delimiter=' ')
+
+
+print 'Data Flattening Completed Successfully'
+
+
+
+
+
+#-------------------------------------------------------------
+#
+# if __name__ == "__main__":
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-t', '--type', required=True, choices=['tiff', 'ascii'], help="input file type (tiff or ascii)  [REQUIRED]")
+#     parser.add_argument('-o', '--output', required=True, help="path to output file [REQUIRED]")
+#     parser.add_argument('-i', '--inputs', required=True, nargs='*', help="input files to process [REQUIRED]")
+#     parser.add_argument('--runtests', action='store_true', help="run tests")
+#     args = parser.parse_args()
+#
+#     if os.path.exists(args.output):
+#         response = raw_input('Output file already exists. Do you want to delete it? [Y/n]')
+#
+#         # delete the file
+#         if response != 'n':
+#             os.remove(args.output)
+#     data = []
+#     if args.type == 'tiff':
+#         for i in range(len(args.inputs)):
+#             data.append(flatten_tiff(args.inputs[i]))
+#             print "%s is written and contains %s values " % (args.inputs[i],len(flatten_tiff(args.inputs[i])))
+#
+#     elif args.type == 'ascii':
+#         for i in range(len(args.inputs)):
+#            data.append(ascii2row(args.inputs[i]))
+#
+#     # write the output
+#     with open(args.output, 'w') as f:
+#         for i in range(len(data)):
+#             if data[i].dtype != 'float':
+#                 data[i] = data[i].astype('float')
+#
+#         # build the output array
+#         outarr = data[0]
+#         for i in range(1, len(data)):
+#             outarr = numpy.hstack([outarr, data[i]])
+#
+#         # save outarr to text file
+#         numpy.savetxt(f, outarr, fmt='%1.3f', delimiter=' ')
+#
+#
+#     print 'Data Flattening Completed Successfully'
