c8f5949cb07275218461ece97f91f29cd7767bec
prasanna310 <dahal.prasanna@gmail.com>
2016-06-17 17:00:37 -0600

    changes to simulation RBC

--- simulations/RBC/run.py
+++ simulations/RBC/run.py
@@ -13,16 +13,16 @@ pytopkapi.run('TOPKAPI.ini')
 
 plot_Qsim_Qobs_Rain.run('plot-flow-precip.ini')
 
-plot_soil_moisture_maps.run('plot-soil-moisture-maps.ini')
-
-# results
-result_sim5 = h5py.File("../../simulations/TEST SIMULAITON5/run_the_model/results/results.h5")
-channel_flows = result_sim5['Channel/Qc_out'][...]
-flow = channel_flows[:,0]
-
-soil_stores = result_sim5['Soil/V_s'][...]
-soil = soil_stores[:, :10]
-
-# channel_flow = result_sim5['Channel']['Qc_out'][:]
-# w, h = channel_flow.shape
-# img = Image.fromarray(channel_flow, 'RGB')
+# plot_soil_moisture_maps.run('plot-soil-moisture-maps.ini')
+#
+# # results
+# result_sim5 = h5py.File("../../simulations/TEST SIMULAITON5/run_the_model/results/results.h5")
+# channel_flows = result_sim5['Channel/Qc_out'][...]
+# flow = channel_flows[:,0]
+#
+# soil_stores = result_sim5['Soil/V_s'][...]
+# soil = soil_stores[:, :10]
+#
+# # channel_flow = result_sim5['Channel']['Qc_out'][:]
+# # w, h = channel_flow.shape
+# # img = Image.fromarray(channel_flow, 'RGB')
