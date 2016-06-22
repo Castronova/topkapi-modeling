9b64c330f610e88c70760d68594934103171b2d5
prasanna310 <dahal.prasanna@gmail.com>
2016-06-21 19:19:57 -0600

    fake rainfall with some value 0

--- preprocessing/scripts/Fake PPT_ET.py
+++ preprocessing/scripts/Fake PPT_ET.py
@@ -4,7 +4,7 @@
 import os, h5py , numpy
 
 # path = "../simulations/TEST SIMULAITON_RBC_demo/run_the_model/forcing_variables"
-# path = r"C:\Users\WIN10-HOME\OneDrive\Public\topkapi-modeling\simulations\TEST SIMULAITON_RBC_demo\run_the_model\forcing_variables"
+# path = r"../../simulations/RBC_2/run_the_model/forcing_variables"
 path = "."
 rainfall_outputFile = os.path.join(path, "rainfields_RBC.h5")
 ET_outputFile = os.path.join(path, "ET_RBC.h5")
@@ -24,6 +24,10 @@ with h5py.File(rainfall_outputFile,'w') as f2:
         a.fill(rainfall_intensity_perUnitTime)  #
         data[i,:] = a
 
+    data[0,:] = numpy.zeros((1,no_of_cell))
+    data[time_step/4,:] = numpy.zeros((1,no_of_cell))
+    data[time_step/2,:] = numpy.zeros((1,no_of_cell))
+    data[time_step/3,:] = numpy.zeros((1,no_of_cell))
     rainArray[:] = data
 
 
